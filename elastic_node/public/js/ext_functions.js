0b0048253234a872f1dced6cd8c86f74049a3491
dnovaes <novaesdigo@gmail.com>
2017-06-07 02:42:43 -0300

    added vue.js file to work in offline mode and a module of supporting functions.

---
+++ elastic_node/public/js/ext_functions.js
@@ -0,0 +1,40 @@
+var lib = {
+  rmEmptySpace: function (keywords){
+    for(var i in keywords){
+      //check for a empty word
+      if((keywords[i] == " ") || (keywords[i] == "")){
+
+        // get the "head" of the array
+        //check if 'i' is the last index of the keywords.length
+        if(parseFloat(i)+1 < keywords.length){
+          var head = [];
+          //check if 'i' isnt the first element of array
+          if(i > 0){
+            head = keywords.slice(i-1, i);
+          }
+
+          // tail of the array (part after the element removed)
+          if((parseFloat(i)+1) == keywords.length){
+            var rest = keywords[keywords.length-1];
+          }else{
+            var rest = keywords.slice(parseFloat(i)+1);
+          }
+          
+          Array.prototype.push.apply(head, rest);
+          keywords = head;
+          keywords = arguments.callee(keywords);
+          return keywords;
+
+        }else{
+          //else: last position of array to remove
+          keywords = keywords.slice(0, keywords.length-1);
+          return keywords;
+        }
+      }
+    }
+    return keywords;
+  }
+
+};
+
+module.exports = lib;
