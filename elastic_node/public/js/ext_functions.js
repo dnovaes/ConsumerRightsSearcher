0c5e919d50c7a77563c68aae819e1ef756ea3ada
dnovaes <novaesdigo@gmail.com>
2017-07-05 15:53:44 -0300

    function commented for future use

--- elastic_node/public/js/ext_functions.js
+++ elastic_node/public/js/ext_functions.js
@@ -1,11 +1,13 @@
 var lib = {
+  // function remove keywords that are equal to a emptyspace and reestructure the array with the
+  // rest of keywords
   rmEmptySpace: function (keywords){
     for(var i in keywords){
       //check for a empty word
       if((keywords[i] == " ") || (keywords[i] == "")){
 
         // get the "head" of the array
-        //check if 'i' is the last index of the keywords.length
+        //check if index 'i' isnt the last index of the keywords.length
         if(parseFloat(i)+1 < keywords.length){
           var head = [];
           //check if 'i' isnt the first element of array
