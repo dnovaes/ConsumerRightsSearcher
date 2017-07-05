7442b3b3235a4db1cd4b5ed1bc1b03579e93ebb8
dnovaes <novaesdigo@gmail.com>
2017-07-05 15:58:49 -0300

    added js file to make requests of search only using the api of elasticsearch.js

--- elastic_node/controller/search.js
+++ elastic_node/controller/search.js
@@ -1,23 +1,44 @@
-var client = require('../connection.js');
+var client            = require('../connection.js');
+//var bodyParser        = require('body-parser');
 
-client.search({
-  index: 'cdc',
-  body:{
-    query:{
-      match: {"content": "establishes"}
-    },
-  }
-}, function(error, response, status){
-  if(error){
-    console.log("search error: "+error);
-  }
-  else{
-    console.log("--- Response ---");
-    console.log(response);
-    console.log("--- Hits ---");
-    response.hits.hits.forEach(function(hit){
-        console.log(hit);
-    })
-  }
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true}));
+
+/*
+app.post('/', function(res, req){
+  data = req.body.claimdata;
+  res.render(data);
 });
+*/
 
+search = function(content, res){
+  var result = [];
+
+  console.log(res);
+
+  client.search({
+    body:{
+      query:{
+        match: {"content": content}
+      },
+    }
+  }, function(error, response, status){
+    if(error){
+      console.log("search error: "+error);
+    }
+    else{
+      console.log("--- Response ---");
+      console.log(response);
+      console.log("--- Hits ---");
+      response.hits.hits.forEach(function(hit){
+          console.log(hit);
+          result.push(hit);
+      })
+
+      res.write("this is a test");
+    }
+  });
+
+}
+
+module.exports = search;
